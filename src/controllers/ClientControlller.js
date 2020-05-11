7009d908f49b5ccea7e953455a9a1985ecaf7fff
Edson Candido <edson.cursoprogramacao@gmail.com>
2020-05-10 21:11:45 -0300

    Ajustes controller Clientes

--- src/controllers/ClientControlller.js
+++ src/controllers/ClientControlller.js
@@ -2,7 +2,7 @@ const Client = require('../models/Client');
 
 module.exports = {
     async index(req, res) {
-        const clients = await User.findAll();
+        const clients = await Client.findAll();
 
         return res.json(clients);
     },
